4ce320f49836b4e3b11fd76559890f3c4be69af4
DefNotMysticS <sebas.gonzalez3003@gmail.com>
2025-09-05 17:53:07 -0500

    QueueDefined

---
+++ structures/Queue.py
@@ -0,0 +1,16 @@
+from structures import Node
+
+
+class Queue:
+    def __init__(self, top):
+        self.top = top
+
+    def getTop(self):
+        return self.top
+    
+    # def push(self, value):
+    #     if self.top is None:
+    #         self.top = Node(value)
+    #     else:
+    #         aux = self.top
+    #         while aux is not None:

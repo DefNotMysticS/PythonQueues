89e5ee4abab7926490cace24d67df8f9e28a1c68
Mateo Castro <tucorreo@example.com>
2025-09-05 17:47:42 -0500

    implement node

---
+++ structures/Node.py
@@ -0,0 +1,17 @@
+class Node:
+    def _init_(self, value, next=None):
+        self.value = value
+        self.next = next
+        
+    def get_value(self):
+        return self.value
+    
+    def set_value(self,value):
+        self.value = value
+        
+    def get_next(self):
+        return self.next
+    
+    def set_next(self, next):
+        self.next = next
+        
